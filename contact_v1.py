c45640067f269da3ec709af610378d2821e0cba0
yl22646 <yl22646@my.westlake.school.nz>
2024-02-19 09:35:16 +1300

    lol

---
+++ contact_v1.py
@@ -0,0 +1,8 @@
+contact_list = {"id_1":
+                    {"first name": "person",
+                     "last name": "person",
+                     "mob": "020",
+                     "email": "adjnla",
+                    }
+                }
+
