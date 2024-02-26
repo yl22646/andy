f9fa2add24c515311f9f2efbe0d907bcb5ecc9c6
yl22646 <yl22646@my.westlake.school.nz>
2024-02-27 09:50:56 +1300

    Update bakery.py

--- bakery.py
+++ bakery.py
@@ -1,35 +1,35 @@
 import easygui
 
 bakery = {'Savouries':
-            {'mice and cheese pie': '$5',
-             'wrap': '$5',
-             'pizza': '$5',
-             'sausage roll': '$4',
-             'spinich feta quiche': '$6'},
+            {'mice and cheese pie': 5,
+             'wrap': 5,
+             'pizza': 5,
+             'sausage roll': 4,
+             'spinich feta quiche': 6},
           
           'Sandwiches':
-            {'ham and cheese': '$6',
-             'roast beef': '$6',
+            {'ham and cheese': 6,
+             'roast beef': 6,
              'blt': '$7',
-             'egg salad': '$6',
-             'vegetarian': '$5'},
+             'egg salad': 6,
+             'vegetarian': 5},
           
           'Sweets':
-            {'chocolate cake': '$4',
-             'lamington': '$4',
-             'muffin': '$4',
-             'pain un chocola': "$4",
-             'danish': "$4"},
+            {'chocolate cake': 4,
+             'lamington': 4,
+             'muffin': 4,
+             'pain un chocola': 4,
+             'danish': 4},
 
           'Drinks':
-            {'coffee': '$6',
-             'hot chocolate': '$5',
-             'soft drink': '$3',
-             'juice': '$2',
-             'smoothie': '$5'},
+            {'coffee': 6,
+             'hot chocolate': 5,
+             'soft drink': 3,
+             'juice': 2,
+             'smoothie': 5},
             
           'Miscellaneous':
-            {'donut': '$4',
+            {'donut': 4,
              'stir fried rice': '$6', 
              'tomato suace': '$4',
              'crossant': '$4',
@@ -46,16 +46,38 @@ def print_menu():
   easygui.msgbox(f'{shop}')
 
 def search_menu():
-  catergory = easygui.buttonbox('which catergory would you like to look through', title="pick catergory", choices=['Savouries', 'Sandwiches', 'Sweets', 'Drinks', 'Miscellaneous'])
+  catgor = easygui.buttonbox('which catergory would you like to look through'
+  , title="pick catergory"
+  , choices=['Savouries', 'Sandwiches', 'Sweets', 'Drinks', 'Miscellaneous'])
   menu = ''
   for i, food in bakery.items():
-    if i == catergory:
+    if i == catgor:
       for price in food:
         menu += (f'\n{price}: {food[price]}')
-  easygui.msgbox(f'{catergory}: \n{menu}')
+  easygui.msgbox(f'{catgor}: \n{menu}')
+
+def search_item():
+  menu = []
+  check = []
+  for category, food in bakery.items():
+    for foods, price in food.items():
+      menu.append(foods)
+  cart = easygui.multchoicebox(f'bakery items', choices= menu)
+  for category, food in bakery.items():
+    for price in food:
+      print(food[price])
+      check.append(food[price])
+  checkout = sum(check)
+  easygui.msgbox(f'Your total is ${checkout}')
   
-choice = easygui.buttonbox("would you like the whole menu list or would you like to search through the menu", choices=["print menu", "search menu"])
+
+  
+choice = easygui.buttonbox('''would you like the whole menu list 
+or would you like to search through the menu?'''
+, choices=["print menu", "search menu", "search_item"])
 if choice == 'print menu':
-    print_menu()
+  print_menu()
 elif choice == 'search menu':
   search_menu()
+elif choice == 'search_item':
+  search_item()
