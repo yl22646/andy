c0fd2139c1b03dfca3fd5d860510b5f49947a52f
yl22646 <yl22646@my.westlake.school.nz>
2024-02-29 14:14:57 +1300

    lol

---
+++ movies.py
@@ -0,0 +1,20 @@
+movies = {
+    'Sherk': {
+        'Genre': 'Fatnasy' 'Animated',
+        'Year released': '18 May 2001',
+        'IMDB rating': '7.9/10',
+        'Type': 'Movie'},
+    
+    'Cars': {
+        'Genre': 'Adventure' 'Animated',
+        'Year released': '9 June 2006',
+        'IMDB rating': '7.2/10',
+        'Type': 'Movie'},
+    
+    'Kunfu Panda': {
+        'Genre': '',
+        'Year released': '',
+        'IMDB rating': '',
+        'Rotten Tomato rating': ''
+    }
+}
