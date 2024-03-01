5cfff4418b1048310486097841662299b7aad518
yl22646 <yl22646@my.westlake.school.nz>
2024-03-02 00:51:47 +1300

    Update movies.py

--- movies.py
+++ movies.py
@@ -3,41 +3,110 @@ import easygui
 movies = {
     'Sherk': {
         'Genre': ['Fatnasy', 'Animated', 'Romance'],
-        'Year released': '18 May 2001',
-        'IMDB rating': '7.9/10',
+        'Year released': '2001',
+        'Rating': '7/10',
         'Type': 'Movie'},
     
     'Cars': {
         'Genre': ['Comedy', 'Animated', 'Adventure'],
-        'Year released': '9 June 2006',
-        'IMDB rating': '7.2/10',
+        'Year released': '2006',
+        'Rating': '7/10',
         'Type': 'Movie'},
     
     'Kunfu Panda': {
         'Genre': ['Animated', 'Adventure', 'Action'],
-        'Year released': '26 June 2008',
-        'IMDB rating': '7.6/10',
+        'Year released': '2008',
+        'Rating': '7/10',
         'Type': 'Movie'},
 
     'Black Panther': {
         'Genre': ['Action', 'Adventure', 'Sci-Fi'],
-        'Year released': '16 February 2018',
-        'IMDB rating': '7.3/10',
+        'Year released': '2018',
+        'Rating': '7/10',
         'Type': 'Movie'}
 }
 
+
+
+
 def print_all():
+    description = ''
+    msg = ''
+    for movie_title, movie_info in movies.items():
+        description = ''
+        for catergory in movie_info:
+            description += (f'\n{catergory}: {movie_info[catergory]}')
+        msg += (f'\n\n{movie_title} \n{description}') 
+    easygui.msgbox(f'{msg}')
+
+
+def movie_search():
     choices = []
     for film in movies:
         choices.append(film)
         msg = 'What movie would you like to check out?'
         title = 'All movies'
-    easygui.multchoicebox(msg, title, choices)
-    
-def movie_search():
-    while True:
-        y = []
+    user_pick = easygui.choicebox(msg, title, choices)
+    msg_1 = ''
+    for movie_title, movie_info in movies.items():
+        if user_pick == movie_title:
+            for catergory in movie_info:
+                msg_1 += (f'\n{catergory}: {movie_info[catergory]}')
+    easygui.msgbox(f'{user_pick}: \n{msg_1}')
 
+
+def add_movie():
+    movie_info = {}
+    
+    message = 'What is the title of the movie?'
+    name = 'Movie title'
+    movie_title = easygui.enterbox(message, name)
+    
+    for movie_name, movie_detail in movies.items():
+        for catergory in movie_detail:
+            if catergory == 'Genre':
+                genre = []
+                choices = ['Sci-Fi', 'Adventure', 'Animated', 'Mystery',
+                           'Romance', 'Drama', 'Action', 'Fantasy',
+                           'comdey', 'Crime', 'Horror', 'Thriller']
+                msg = 'What are the genres?'
+                title = 'Genres'
+                user_choice = easygui.multchoicebox(msg, title, choices)
+                genre.append(user_choice)
+                selection = {catergory: user_choice}
+                movie_info.update(selection)
+            
+            elif catergory == 'Year released':
+                msg = 'When was the movie released?'
+                title = 'Year released'
+                user_choice = easygui.integerbox(msg, title)
+                selection = {catergory: user_choice}
+                movie_info.update(selection)
+            
+            elif catergory == 'Rating':
+                msg = "What rating would you give it out of 10?"
+                title = "Rating"
+                option = 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+                user_choice = easygui.buttonbox(msg, title, option)
+                selection = {catergory: user_choice/10}
+                movie_info.update(selection)
+            
+            elif catergory == "Type":
+                msg = 'Is it a movie or a Tv Series'
+                title = 'Type'
+                option = 'Movie', 'Tv Series'
+                user_choice = easygui.choicebox(msg, title, option)
+                selection = {catergory: user_choice}
+                movie_info.update(selection)
+    
+    movie_description = (f'\n{movie_info}')
+    new_movie = {movie_title: movie_description}
+    easygui.msgbox(new_movie)
+    movies.update(new_movie)
+
+
+  
+            
 def leave():
     easygui.msgbox("Goodbye")
     return "n"
@@ -46,7 +115,9 @@ def leave():
 options = {
     'Print all': print_all,
     'Search movie': movie_search,
-    'Exit': leave,
+    'Add movie': add_movie,
+    'Exit': leave
+    
 }
 
 get_input = "y"
