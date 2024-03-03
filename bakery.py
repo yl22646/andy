1a5e9f57b87409f312cf03a81ae79f289e1b08ec
yl22646 <yl22646@my.westlake.school.nz>
2024-03-04 12:00:29 +1300

    Update bakery.py

--- bakery.py
+++ bakery.py
@@ -73,8 +73,51 @@ def search_item():
           check.append(food[price])
   checkout = sum(check)
   easygui.msgbox(f'Your total is ${checkout}')
-  
 
+def add_item():
+    output = ''
+    catergories = []
+    msg = ''
+    title = ''
+
+    for bakery_catergories in bakery:
+      catergories.append(bakery_catergories)
+    catergory_choice = easygui.buttonbox(msg, title,catergories)
+
+    msg = f"please enter the item you would like to add to the \
+    {catergory_choice}"
+    title = ''
+    new_item = easygui.enterbox(msg,title)
+
+    msg = f"please enter the item you would like to add to the \
+    {new_item}"
+    title = ''
+    new_price = easygui.enterbox(msg,title)
+    
+    while True:
+        bakery[catergory_choice][new_item] = new_price
+
+def update():
+  output = ''
+  msg = ''
+  title = ''
+  for bakery_catergories in bakery:
+    catergories.append(bakery_catergories)
+
+  catergory_choice = easygui.buttonbox(msg, title, catergories)
+
+  bakery_items = []
+  msg = ''
+  title = ''
+
+  for items in bakery[catergory_choice]:
+    bakery_items.append(items)
+  item_choice = easygui.buttonbox(msg, title, bakery_items)
+
+  msg = f'enter new price for {item_choice}'
+  title = ''
+
+  bakery[catergory_choice][item_choice]
   
 choice = easygui.buttonbox('''would you like the whole menu list 
 or would you like to search through the menu?'''
@@ -85,3 +128,5 @@ elif choice == 'search menu':
   search_menu()
 elif choice == 'search_item':
   search_item()
+elif choice == 'add_item':
+  add_item()
