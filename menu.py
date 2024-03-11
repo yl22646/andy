db1a16cdab6e3ea999a8f13ef94f4aca853d7ea6
yl22646 <yl22646@my.westlake.school.nz>
2024-03-12 12:00:22 +1300

    lol

--- menu.py
+++ menu.py
@@ -1,3 +1,5 @@
+import easygui
+
 menu = {"Value" : {
     "Beef Burger" : 5.69,
     "Fries" : 1.00,
@@ -12,3 +14,21 @@ menu = {"Value" : {
     "Cheese Burger" : 6.69,
     "Large Fries" : 2.00,
     "Smoothie" : 2.00}}
+
+def print_all():
+    '''When called this function will print all combos and it's information
+    on easygui'''
+    output = ''
+    total_price = []
+    msg = ''
+    
+    for combo_name, combo_info in menu.items():
+        output = ''
+        for price in combo_info:
+            total_price.append(combo_info[price])
+            output += (f'\n{price}: ${combo_info[price]: .2f}')
+        add_total = sum(total_price)
+        msg += (f'\n\n{combo_name} \n{output} \n${add_total: .2f}') 
+    easygui.msgbox(f'{msg}')    
+
+print_all()
